14be9ba5664cac315666f8ac766efb0a3f50aba4
yijiechong13 <195323790+yijiechong13@users.noreply.github.com>
2025-08-19 21:42:57 +0800

    Add greeting

---
+++ src/main/java/Octopus.java
@@ -0,0 +1,11 @@
+public class Octopus {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Octopus");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
